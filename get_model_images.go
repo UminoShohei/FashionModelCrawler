3bc68731af609a6f2ec612062cbfafcb33a1ddbf
UminoShohei <umino.shohei@gmail.com>
2017-04-09 00:12:26 +0900

    add csv import

--- get_model_images.go
+++ get_model_images.go
@@ -1,39 +1,65 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	j := 1
-	for i := 1; i < 20; i++ {
-		doc, err := goquery.NewDocumentFromResponse(getImages(i))
-		fmt.Println(doc.Url)
+	readCSV()
+
+}
+func readCSV() {
+	var fp *os.File
+	if len(os.Args) < 2 {
+		fp = os.Stdin
+	} else {
+		var err error
+		fp, err = os.Open(os.Args[1])
 		if err != nil {
-			fmt.Print("url scarapping failed")
+			panic(err)
 		}
-		doc.Find("img").Each(func(_ int, s *goquery.Selection) {
-			url, _ := s.Attr("src")
-			saveImage(url, j)
-			j++
-			fmt.Println(url)
-		})
+		defer fp.Close()
+	}
+
+	reader := csv.NewReader(fp)
+	reader.Comma = ','
+	reader.LazyQuotes = true // ダブルクオートを厳密にチェックしない！
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		j := 1
+		for i := 1; i < 20; i++ {
+			doc, err := goquery.NewDocumentFromResponse(getImages(record[0], record[1], i))
+			fmt.Println(doc.Url)
+			if err != nil {
+				fmt.Print("url scarapping failed")
+			}
+			doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+				url, _ := s.Attr("src")
+				saveImage(record[0], url, j)
+				j++
+				fmt.Println(url)
+			})
+		}
 	}
 }
-
-func getImages(n int) *http.Response {
-	url := fmt.Sprintf("http://gensun.org/?mode=ajax&q=小松菜奈&pid=2282199&page=%d&size=100&safe=on&sort=", n)
-	fmt.Println(url)
+func getImages(name, pid string, n int) *http.Response {
+	url := fmt.Sprintf("http://gensun.org/?mode=ajax&q=%s&pid=%s&page=%d&size=100&safe=on&sort=", name, pid, n)
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Host", "gensun.org")
-	req.Header.Set("Referer", "http://gensun.org/wid/2282199") //ここ引数で取れるようにするべきかな
+	req.Header.Set("Referer", fmt.Sprintf("http://gensun.org/?q=%s", name))
 	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 	req.Header.Set("Host", "gensun.org")
 	client := new(http.Client)
@@ -41,15 +67,11 @@ func getImages(n int) *http.Response {
 	return resp
 }
 
-func saveImage(url string, i int) {
-	fmt.Println(url)
-
+func saveImage(name, url string, i int) {
 	if strings.Compare(url, "/img/loading.gif") == 0 {
-		fmt.Println("AAAAAAAAAAAA")
 		return
 	}
 	defer func() {
-		fmt.Println("End")
 		err := recover()
 		if err != nil {
 			fmt.Println("Recover!:", err)
@@ -60,7 +82,7 @@ func saveImage(url string, i int) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	imageName := fmt.Sprintf("%d.jpg", i)
+	imageName := fmt.Sprintf("/Users/uminoshohei/Project/go/src/github.com/UminoShohei/get_model_images/hoge/%s_%d.jpg", name, i)
 	defer response.Body.Close()
 	file, err := os.Create(imageName)
 	if err != nil {
